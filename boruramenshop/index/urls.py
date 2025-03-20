4ba22dadb7f571d664330ae49ca45ab864cd97ec
KrystianAnd <krystianandrzejak7@gmail.com>
2025-03-21 00:10:11 +0100

    some updates

--- boruramenshop/index/urls.py
+++ boruramenshop/index/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns =[
     path('', views.main, name='main'),
+    path('menu/', views.menu, name='menu'),
+    path('polityka-strony/', views.PolitykaStrony, name='polityka_strony'),
 ]
--- boruramenshop/index/views.py
+++ boruramenshop/index/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request, 'main.html')
+def menu(request):
+    return render(request, 'menu.html')
+
+def PolitykaStrony(request):
+    return render(request, 'polityka_strony.html')

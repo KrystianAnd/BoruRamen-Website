35f2c96319aa830ef6712b66e47d748539e18432
KrystianAnd <krystianandrzejak7@gmail.com>
2025-04-08 23:07:00 +0200

    version given to client
    The version that is full done.

--- boruramenshop/boruramenshop/settings.py
+++ boruramenshop/boruramenshop/settings.py
@@ -11,24 +11,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+from django.utils.translation import gettext_lazy as _
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0^_7^t(nroz_#js+)ab9d(d=l49&om*-*qc82(jy=h-yvh1b^x'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,8 +28,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'index',
+    'django_check_seo',
+    'cms',
+    'menus',
+    'treebeard',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -72,20 +70,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'boruramenshop.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -102,25 +92,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
-
 TIME_ZONE = 'UTC'
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-from django.utils.translation import gettext_lazy as _
 
 LANGUAGES = [
     ('en', _('English')),
@@ -136,3 +112,5 @@ USE_TZ = True
 LOCALE_PATHS = [
     BASE_DIR / 'locale/', 
 ]
+
+CMS_CONFIRM_VERSION4 = True
--- boruramenshop/index/urls.py
+++ boruramenshop/index/urls.py
@@ -1,9 +1,11 @@
 from index import views
 from django.urls import path
+from .views import robots_txt
 
 urlpatterns = [
     path('', views.main, name='main'),
     path('menu/', views.menu, name='menu'),
     path('polityka-strony/', views.PolitykaStrony, name='polityka_strony'),
-    path('set-language/', views.set_language, name='set_language'),  # ✅ Dodajemy obsługę zmiany języka
+    path('set-language/', views.set_language, name='set_language'),  
+    path("robots.txt", robots_txt, name="robots_txt"),
 ]
